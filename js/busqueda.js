ada74af0862235782c2bf14ab0f7355c751674ae
Christian Zanger <christian.zanger@estudiants.urv.cat>
2016-02-26 21:06:02 +0100

    anuncis

--- js/busqueda.js
+++ js/busqueda.js
@@ -2,11 +2,16 @@ var resizeInput = function(){
      $('.busqueda').css({'margin-top': $(window).height()/2 + 'px'});
 };
 
+createAnuncis = function(){
+	$('.anuncis').animate({'opacity':'1'},800);
+}
+
 var moveSearchBox = function(){
 	$('.busqueda').animate({'margin-top':25+'px'},800);
 	$('.busqueda').find("input").animate({'height': 40 + 'px','font-size': '100%'},800);
 	$('.search-button').animate({'padding': 4 + 'px'},800);
 	$('.bg').animate({'height': '100' + 'px'},800);
+	createAnuncis();
 }
 
 
